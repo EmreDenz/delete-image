9bb58839d06e9eb78d1890f7eb369dd9e09535b8
Emre Deniz <emre_denz@hotmail.com>
2024-02-13 13:24:02 +0300

    updated

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 docker build -t docker.pkg.github.com/emredenz/delete-deneme-image/test:1.0.0 .
 docker build -t docker.pkg.github.com/emredenz/delete-deneme-image/test:1.0.0-RC0 . -f Dockerfile1
-docker build -t docker.pkg.github.com/emredenz/delete-deneme-image/test:1.0.0-RC0 . -f Dockerfile2
+docker build -t docker.pkg.github.com/emredenz/delete-deneme-image/test:1.0.0-RC1 . -f Dockerfile2
 docker push docker.pkg.github.com/emredenz/delete-deneme-image/test:1.0.0
 docker push docker.pkg.github.com/emredenz/delete-deneme-image/test:1.0.0-RC0
 docker push docker.pkg.github.com/emredenz/delete-deneme-image/test:1.0.0-RC1
